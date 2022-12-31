822dacfcb50aa6f2bd28c7cd3f40b9ffc291d09f
Jaiswar <jaiswarnilesh2002@gmail.com>
2022-12-31 18:57:26 +0530

    updated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,18 +4,23 @@ import "../styles/css/index.css"
 import { Provider } from 'react-redux';
 import { PersistGate } from "redux-persist/integration/react";
 import { persistor, store } from '../redux';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
 
   return (
-    // 2. Use at the root of your app
-    <NextUIProvider>
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor} >
-          <Component {...pageProps} />
-        </PersistGate>
-      </Provider>
-    </NextUIProvider>
+    <>
+      <Head>
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+      </Head>
+      <NextUIProvider>
+        <Provider store={store}>
+          <PersistGate loading={null} persistor={persistor} >
+            <Component {...pageProps} />
+          </PersistGate>
+        </Provider>
+      </NextUIProvider>
+    </>
   );
 }
 
